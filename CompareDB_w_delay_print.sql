3dbbb8037ac3cdd2d1b697c99605ac7366f8acc8
shaulliv <shaulliv@gmail.com>
2021-07-07 18:25:38 +0300

    Create CompareDB_w_delay_print.sql

---
+++ CompareDB_w_delay_print.sql
@@ -0,0 +1,17 @@
+/* checkes running count field two seconds apart.
+If the two values are the same the table is not being written to.
+Good for DB's that are being writte to havily all the time.
+outputs result as print */
+DECLARE @segid1 INT
+DECLARE @segid2 INT
+select top 1 @segid1=[RunningCountField] from [DBName] order by startdatetime desc
+--debugging line
+--SELECT @segid1
+waitfor delay '00:00:02';
+select top 1 @segid2=[RunningCountField] from [DBName] order by startdatetime desc
+--debugging line
+--SELECT @segid2,@segid1
+IF (@segid1 != @segid2)
+	PRINT 1;
+ELSE IF (@segid1 = @segid2)
+	PRINT 0;
