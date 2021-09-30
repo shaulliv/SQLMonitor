47545312de7c8fabc273f2b3d8f9b50b4c583b57
shaulliv <shaulliv@gmail.com>
2021-09-30 16:33:49 +0300

    Update CompareDB_30_min_UTC_print.sql

--- CompareDB_30_min_UTC_print.sql
+++ CompareDB_30_min_UTC_print.sql
@@ -4,10 +4,10 @@ Good for DB's that are not being writte to havily all the time (i.e. low volume 
 outputs results as print */
 DECLARE @segid1 INT
 DECLARE @segid2 INT
-select top 1 @segid1=[RunningCountField] from [DBName] order by dateandtime desc
+select top 1 @segid1=[RunningCountField] from [DBName] order by [dateandtime] desc
 --debugging line
 --SELECT @segid1
-select top 1 @segid2=[RunningCountField] from [DBName] where dateandtime < dateadd(minute, -30, GETUTCDATE()) order by dateandtime desc
+select top 1 @segid2=[RunningCountField] from [DBName] where [dateandtime] < dateadd(minute, -30, GETUTCDATE()) order by [dateandtime] desc
 --debugging line
 --SELECT @segid2,@segid1
 IF (@segid1 != @segid2)
