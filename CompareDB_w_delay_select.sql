3d74b17340cdb72237ab18ea005a0dc1d70014b3
shaulliv <shaulliv@gmail.com>
2021-09-30 16:31:33 +0300

    Update CompareDB_w_delay_select.sql

--- CompareDB_w_delay_select.sql
+++ CompareDB_w_delay_select.sql
@@ -3,11 +3,11 @@ If the two values are the same the table is not being written to.
 Good for DB's that are being writte to havily all the time */
 DECLARE @segid1 INT
 DECLARE @segid2 INT
-select top 1 @segid1=[RunningCountField] from [DBName] order by startdatetime desc
+select top 1 @segid1=[RunningCountField] from [DBName] order by [dateandtime] desc
 --debugging line
 --SELECT @segid1
 waitfor delay '00:00:02';
-select top 1 @segid2=[RunningCountField] from [DBName] order by startdatetime desc
+select top 1 @segid2=[RunningCountField] from [DBName] order by [dateandtime] desc
 --debugging line
 --SELECT @segid2,@segid1
 IF (@segid1 != @segid2)
